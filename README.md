2cab7db8507ed209f92210fc62635d8bb68b0fa9
Naqiya Nullwala <nullwalanaqiya@gmail.com>
2026-08-16 00:22:54 +0530

    Add subtraction and division

---
+++ README.md
@@ -0,0 +1,4 @@
+Name: Naqiya Nullwala
+Branch: IT Engineering
+Subject: Open Source Development
+Practical No.: 2
---
+++ calculator.py
@@ -0,0 +1,11 @@
+def subtraction(a, b):
+    return a - b
+
+def division(a, b):
+    return a / b
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Subtraction:", subtraction(num1, num2))
+print("Division:", division(num1, num2))
